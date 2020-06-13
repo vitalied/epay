03a80f441649191c93e649a904ea9bc78ca21d32
Vitalie D <vitalie.dev@gmail.com>
2020-06-13 20:56:08 +0300

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,75 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(email: 'admin@dev.local', password: '123456', admin: true)
+
+Admin::MerchantCreator.call(name: 'Merchant 1', email: 'merchant1@dev.local', status: Merchant::STATUS.active)
+Admin::MerchantCreator.call(name: 'Merchant 2', email: 'merchant2@dev.local', status: Merchant::STATUS.inactive)
+
+user = Merchant.first.user
+user.password = '123456'
+user.authentication_token = '123456'
+user.save!
+user = Merchant.last.user
+user.password = '654321'
+user.authentication_token = '654321'
+user.save!
+
+Transaction::STATUSES.each do |status|
+  AuthorizeTransaction.create!(
+    merchant: Merchant.first,
+    uuid: SecureRandom.uuid,
+    amount: 29.99,
+    status: Transaction::STATUS.send(status),
+    customer_email: 'customer1@dev.local',
+    customer_phone: '+5551111111'
+  )
+end
+
+authorize_transaction = AuthorizeTransaction.create!(
+  merchant: Merchant.first,
+  uuid: SecureRandom.uuid,
+  amount: 9.99,
+  status: Transaction::STATUS.approved,
+  customer_email: 'customer2@dev.local',
+  customer_phone: '+5552222222'
+)
+
+charge_transaction = ChargeTransaction.create!(
+  merchant: Merchant.first,
+  authorize_transaction: authorize_transaction,
+  uuid: SecureRandom.uuid,
+  amount: 9.99,
+  status: Transaction::STATUS.approved,
+  customer_email: 'customer2@dev.local',
+  customer_phone: '+5552222222'
+)
+
+refund_transaction = RefundTransaction.create!(
+  merchant: Merchant.first,
+  charge_transaction: charge_transaction,
+  uuid: SecureRandom.uuid,
+  amount: 9.99,
+  status: Transaction::STATUS.approved,
+  customer_email: 'customer2@dev.local',
+  customer_phone: '+5552222222'
+)
+
+authorize_transaction = AuthorizeTransaction.create!(
+  merchant: Merchant.first,
+  uuid: SecureRandom.uuid,
+  amount: 19.99,
+  status: Transaction::STATUS.approved,
+  customer_email: 'customer3@dev.local',
+  customer_phone: '+5553333333'
+)
+
+reversal_transaction = ReversalTransaction.create!(
+  merchant: Merchant.first,
+  authorize_transaction: authorize_transaction,
+  uuid: SecureRandom.uuid,
+  status: Transaction::STATUS.approved,
+  customer_email: 'customer3@dev.local',
+  customer_phone: '+5553333333'
+)
