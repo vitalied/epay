56790b7db1e4f26d8016360c12e03a1642cac353
Vitalie D <vitalie.dev@gmail.com>
2020-06-13 23:40:33 +0300

    Exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
+  include ExceptionHandling
+
   before_action :authenticate_user!
 end
---
+++ app/controllers/concerns/exception_handling.rb
@@ -0,0 +1,24 @@
+module ExceptionHandling
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActionController::InvalidAuthenticityToken, with: :render_unauthorized
+
+    private
+
+    def render_unauthorized
+      render_errors(:Unauthorized, :unauthorized)
+    end
+
+    def render_errors(error, status = :unprocessable_entity)
+      respond_to do |format|
+        format.html do
+          redirect_to root_path, alert: error
+        end
+        format.json do
+          render json: { error: error }, status: status
+        end
+      end
+    end
+  end
+end
