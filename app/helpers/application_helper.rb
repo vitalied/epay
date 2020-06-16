b919a7d48d6aea7464ca27fb4899e2c4b10394fb
Vitalie D <vitalie.dev@gmail.com>
2020-06-16 13:36:16 +0300

    Shared views and CSS styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@ module ApplicationHelper
   def app_name
     Settings.app_name
   end
+
+  def flash_alert_class(key)
+    case key
+    when 'notice'
+      :success
+    when 'alert'
+      :danger
+    else
+      :info
+    end
+  end
 end
